971e68a6c443e844758ce7e1a503921f52a59d0f
James-Lyu06 <jameslyu06@outlook.com>
2026-07-02 22:22:10 -0700

    Fix Supabase project URL

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.BADMINTON_CONFIG = {
-  SUPABASE_URL: "https://novehfbyvwqthsyetjmn.supabase.co",
+  SUPABASE_URL: "https://novehfbywvqthsyetjmn.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_K_zBSJVp2nOoiMA7lr6_Dg_rhfDMSQC",
   SUBMISSIONS_TABLE: "badminton_submissions"
 };
