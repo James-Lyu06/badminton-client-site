f6237e6cf8b600c433d3773577205441dd319046
James-Lyu06 <jameslyu06@outlook.com>
2026-07-02 21:48:26 -0700

    Add client Supabase config

---
+++ config.js
@@ -0,0 +1,5 @@
+window.BADMINTON_CONFIG = {
+  SUPABASE_URL: "https://novehfbyvwqthsyetjmn.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_K_zBSJVp2nOoiMA7lr6_Dg_rhfDMSQC",
+  SUBMISSIONS_TABLE: "badminton_submissions"
+};
